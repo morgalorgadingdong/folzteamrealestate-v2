ae2325d69104bb9b22b40063cea1a59025f2a5a9
Morgan <morganfolz@gmail.com>
2023-03-06 11:22:22 -0800

    added real logo to footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,10 +1,14 @@
-<footer class="gray flexCenterV">
-    <div class="flexCenterH ninety center flexSpaceBetweenH">
-        <div class="flexLeft flexColumn flexCenterH whiteText flexWrap">
+<footer class="gray flexCenterV py-5">
+    <div class="col-10 d-flex justify-content-center justify-content-sm-between center flex-wrap">
+        <div class="col-8 col-sm-4 col-md-3 order-sm-2">
+            <img class="col-12" src="./img/real-broker-logo-whitetext.png" alt="REAL Brokers">
+        </div>
+        <div class="col-6 col-sm-3 flexLeft flexColumn flexCenterH whiteText flexWrap order-sm-1 my-3 my-sm-0">
             <p class="testimonialText">Folz Team Real Estate</p>
             <p class="testimonialText">2023</p>
         </div>
-        <div class="flexRight flexCenterV flexSpaceAroundH">
+        
+        <div class="col-6 col-sm-3  flexRight flexCenterV flexSpaceAroundH order-sm-3">
             <i class="fa-brands fa-instagram whiteText footerSocial" onclick="location.href='https://www.instagram.com/folzteamrealestate/';" style="cursor: pointer;"></i>
             <i class="fa-brands fa-facebook whiteText footerSocial" onclick="location.href='https://www.facebook.com/folzteamrealestate/';" style="cursor: pointer;"></i>
             <i class="fa-solid fa-envelope whiteText footerSocial" onclick="location.href='/contact';" style="cursor: pointer;"></i>
