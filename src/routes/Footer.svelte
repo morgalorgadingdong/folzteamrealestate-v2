2e10a7edde4d4682134cb8050b5eaf39bce7c5fa
Morgan <morganfolz@gmail.com>
2023-03-06 11:37:36 -0800

    reformatted footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,14 +1,17 @@
 <footer class="gray flexCenterV py-5">
     <div class="col-10 d-flex justify-content-center justify-content-sm-between center flex-wrap">
-        <div class="col-8 col-sm-4 col-md-3 order-sm-2">
+        <div class="col-8 col-sm-4 col-md-3 order-sm-2 mb-5 mb-sm-0">
             <img class="col-12" src="/img/real-broker-logo-whitetext.png" alt="REAL Brokers">
         </div>
-        <div class="col-6 col-sm-3 flexLeft flexColumn flexCenterH whiteText flexWrap order-sm-1 my-3 my-sm-0">
+
+        <div class="col-6 col-sm-3 flexLeft flexColumn flexCenterH whiteText flexWrap order-sm-1 ">
             <p class="testimonialText">Folz Team Real Estate</p>
+            <p class="testimonialText">christinefolzre@gmail.com</p>
+            <p class="testimonialText">(503) 949-3087</p>
             <p class="testimonialText">2023</p>
         </div>
         
-        <div class="col-6 col-sm-3  flexRight flexCenterV flexSpaceAroundH order-sm-3">
+        <div class="col-6 col-sm-3  justify-content-end justify-content-sm-around align-items-center align-content-sm-center flexCenterV  order-sm-3">
             <i class="fa-brands fa-instagram whiteText footerSocial" onclick="location.href='https://www.instagram.com/folzteamrealestate/';" style="cursor: pointer;"></i>
             <i class="fa-brands fa-facebook whiteText footerSocial" onclick="location.href='https://www.facebook.com/folzteamrealestate/';" style="cursor: pointer;"></i>
             <i class="fa-solid fa-envelope whiteText footerSocial" onclick="location.href='/contact';" style="cursor: pointer;"></i>
